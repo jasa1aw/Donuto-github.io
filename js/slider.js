1ae0db657692add2f2ea50c3f2bce15ecf57f60f
jasa1aw <erakrut05@gmail.com>
2024-01-12 16:10:10 +0600

    Initial commit

---
+++ js/slider.js
@@ -0,0 +1,11 @@
+
+let dotsButton = document.getElementsByClassName("dots")
+$('.home-slider').slick({
+    vertical: true,
+    verticalSwiping: true,
+    slidesToShow: 1,
+    autoplay: true,
+    autoplaySpeed: 1000,
+    dots: true,
+});
+
